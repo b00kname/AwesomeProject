7bf3388b1a24b494c9be266bdccb34e002f3d066
BookName <bookname1996@gmail.com>
2018-08-06 17:23:40 +0800

    Format document

--- task-to-go/login.js
+++ task-to-go/login.js
@@ -1,23 +1,23 @@
-import React, {Component} from 'react';
-import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
+import React, { Component } from 'react';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 import Logo from './logo';
 import Form from './form';
 
 // added another comment
-export default class login extends Component {   
+export default class login extends Component {
     static navigationOptions = {
         title: "Login"
     }
 
-    render() {        
-        return(
-            <View style= {styles.container}>
-                <Logo/>
-                <Form type= 'Login'/>
-                <View style= {styles.signupTextCont}>
-                    <Text style= {styles.signupText}>Don't have an account yet?</Text>
-                    <TouchableOpacity onPress= {()=> {this.props.navigation.navigate('Signup')}}>
-                        <Text style= {styles.signupButton}>Signup</Text>
+    render() {
+        return (
+            <View style={styles.container}>
+                <Logo />
+                <Form type='Login' />
+                <View style={styles.signupTextCont}>
+                    <Text style={styles.signupText}>Don't have an account yet?</Text>
+                    <TouchableOpacity onPress={() => { this.props.navigation.navigate('Signup') }}>
+                        <Text style={styles.signupButton}>Signup</Text>
                     </TouchableOpacity>
                 </View>
             </View>
@@ -25,7 +25,7 @@ export default class login extends Component {
     }
 }
 
-const styles= StyleSheet.create ({
+const styles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: 'center',
--- task-to-go/signup.js
+++ task-to-go/signup.js
@@ -1,25 +1,25 @@
-import React, {Component} from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React, { Component } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 import Logo from './logo';
 import Form from './form'
 
 // Added comment
 export default class signup extends Component {
     render() {
-        return(
-            <View style= {styles.container}>
-                <Logo/>
-                <Form type= 'Signup'/>
-                <View style= {styles.signupTextCont}>
-                    <Text style= {styles.signupText}>Already have an account?</Text>
-                    <Text style= {styles.signupButton} onPress={()=>this.props.navigation.goBack()}>Sign in</Text>
+        return (
+            <View style={styles.container}>
+                <Logo />
+                <Form type='Signup' />
+                <View style={styles.signupTextCont}>
+                    <Text style={styles.signupText}>Already have an account?</Text>
+                    <Text style={styles.signupButton} onPress={() => this.props.navigation.goBack()}>Sign in</Text>
                 </View>
             </View>
         );
     }
 }
 
-const styles= StyleSheet.create ({
+const styles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: 'center',
